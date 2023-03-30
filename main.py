30e38b846b2b081d6bce6ca0a314a43e54e00289
Ryu_Ronin <creative.ryu@web.de>
2023-03-30 15:55:05 +0200

    created spaceship class

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroid Shooter 2.0 OOP")
 game_clock = pygame.time.Clock()
+background = pygame.image.load("ressources/graphics/background.png").convert()
 
 # Sprite Groups
 ship_group = pygame.sprite.Group()
 
 # Ship Creation
-ship = SpaceShip()
-ship_group.add(ship)
+ship = SpaceShip(ship_group)
 
 # Game Loop
 while True:
@@ -29,6 +29,7 @@ while True:
     delta_time = game_clock.tick(FPS) / 1000
     
     # Update graphics
+    display.blit(background, (0,0))
     ship_group.draw(display)
 
     # show current Frame
--- spaceship.py
+++ spaceship.py
@@ -2,7 +2,7 @@ import pygame
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 
 class SpaceShip(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, group) -> None:
+        super().__init__(group)
         self.image = pygame.image.load("ressources/graphics/ship.png").convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
